34a69f816ca9139f813dde0168fee5ec7585995a
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2023-02-15 09:52:15 +0900

    update pybind11 to v2.10.3

--- cmake/pybind11.cmake
+++ cmake/pybind11.cmake
@@ -2,6 +2,6 @@ include(FetchContent)
 FetchContent_Declare(
   pybind11
   GIT_REPOSITORY https://github.com/pybind/pybind11.git
-  GIT_TAG        f7b499615e14d70ab098a20deb0cdb3889998a1a # v2.8.1
+  GIT_TAG        0bd8896a4010f2d91b2340570c24fa08606ec406 # v2.10.3
 )
 list(APPEND FetchContents pybind11)
