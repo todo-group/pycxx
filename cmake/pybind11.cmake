35d8e3a4696bb783a27047ff9b38fc4f7babfec8
mosfet80 <realeandrea@yahoo.it>
2023-09-09 00:53:52 +0200

    Update pybind11.cmake
    Update pybind11 to v2.10.4

--- cmake/pybind11.cmake
+++ cmake/pybind11.cmake
@@ -2,6 +2,6 @@ include(FetchContent)
 FetchContent_Declare(
   pybind11
   GIT_REPOSITORY https://github.com/pybind/pybind11.git
-  GIT_TAG        0bd8896a4010f2d91b2340570c24fa08606ec406 # v2.10.3
+  GIT_TAG 5b0a6fc2017fcc176545afe3e09c9f9885283242 #2.10.4
 )
 list(APPEND FetchContents pybind11)
