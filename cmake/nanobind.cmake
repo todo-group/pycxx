309fbef64a9ba58f852fbae74cd1fb65ad3a8801
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2024-11-27 19:25:37 +0900

    cmake: update

--- cmake/nanobind.cmake
+++ cmake/nanobind.cmake
@@ -4,6 +4,6 @@ include(FetchContent)
 FetchContent_Declare(
   nanobind
   GIT_REPOSITORY https://github.com/wjakob/nanobind.git
-  GIT_TAG 784efa2a0358a4dc5432c74f5685ee026e20f2b6 # v2.2.0
+  GIT_TAG        v2.2.0
 )
 list(APPEND FetchContents nanobind)
--- cmake/pybind11.cmake
+++ cmake/pybind11.cmake
@@ -4,6 +4,6 @@ include(FetchContent)
 FetchContent_Declare(
   pybind11
   GIT_REPOSITORY https://github.com/pybind/pybind11.git
-  GIT_TAG a2e59f0e7065404b44dfe92a28aca47ba1378dc4 # v2.13.6
+  GIT_TAG        v2.13.6
 )
 list(APPEND FetchContents pybind11)
